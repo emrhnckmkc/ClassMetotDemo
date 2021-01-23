a5877e93ce50c20d70fab3f02c87656a35996888
Emirhan <Enbiya@LENOVO>
2021-01-23 19:17:29 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+     
+            public void Ekle(Customer musteriEkle)
+            {
+                Console.WriteLine("Müşteriniz Sisteme Eklenmiştir. : " + musteriEkle.Name + " " + musteriEkle.Surname);
+            }
+
+            public void Liste(Customer musteriListe)
+            {
+                Console.WriteLine("Müşterileriniz Listelenmiştir. -> " + musteriListe.Name + " " + musteriListe.Surname + "-" + musteriListe.Age );
+            }
+
+            public void Silme(Customer musteriSilme)
+            {
+                Console.WriteLine("Müşteriniz Sistemden Silinmiştir. : " + musteriSilme.Name + " " + musteriSilme.Surname);
+            }
+
+        }
+    }
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+
+            customer1.Name = "Joseph";
+            customer1.Surname = "Alonso";
+            customer1.Age = 21;
+
+            Customer customer2 = new Customer();
+
+            customer2.Name = "Beth";
+            customer2.Surname = "Harmon";
+            customer2.Age = 29;
+
+            Customer customer3 = new Customer();
+
+            customer3.Name = "Arsène";
+            customer3.Surname = "Lupin";
+            customer3.Age = 45;
+
+            Customer[] customers = new Customer[] { customer1, customer2, customer3 };
+
+
+            foreach (Customer musteri in customers)
+            {
+                Console.WriteLine(musteri.Name);
+                Console.WriteLine(musteri.Surname);
+                Console.WriteLine(musteri.Age);
+
+            }
+
+
+            CustomerManager musteriManager = new CustomerManager();
+
+            musteriManager.Ekle(customer1);
+            musteriManager.Ekle(customer2);
+            musteriManager.Ekle(customer3);
+
+            musteriManager.Liste(customer1);
+            musteriManager.Liste(customer2);
+            musteriManager.Liste(customer3);
+
+
+            musteriManager.Silme(customer1);
+            musteriManager.Silme(customer2);
+            musteriManager.Silme(customer3);
+
+
+        }
+
+    }
+
+}
+
+
+
+        
